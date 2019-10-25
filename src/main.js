1450bf0dc22d624cb4b36d3a4483cd88bb96e4c2
MaYQ <1105052919@qq.com>
2019-10-25 15:37:32 +0800

    初始化目录

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 引入elementUi
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+// 引入vue-router
+import router from '@/router'
+
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 new Vue({
+  // 挂载router
+  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,10 @@
+// 路由组件文件
+import VueRouter from 'vue-router'
+// 引入vue
+import Vue from 'vue'
+Vue.use(VueRouter)
+
+// 实例化
+export default new VueRouter({
+  routes: []
+})
